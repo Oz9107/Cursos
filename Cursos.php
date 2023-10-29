8130c314224a8007978a8a2071a7e4442ba7226c
Oscar Vallejo <oscarvallejoipuz@gmail.com>
2023-10-29 02:15:31 -0500

    Cursos

--- Cursos.php
+++ Cursos.php
@@ -1,6 +1,6 @@
 <?php
 
-// Array simulado de cursos (puedes modificar o conectar a una base de datos)
+// Array
 $cursos = array(
     array(
         'id' => 1,
@@ -20,18 +20,17 @@ $cursos = array(
         'enddate' => '2022-06-30',
         'category' => 'Informática'
     ),
-    // Agregar más cursos según sea necesario
+    // Podemos Agregar más cursos según sea necesario
 );
 
-// Obtener parámetros de la URL (simulados para este ejemplo)
+// Obtener parámetros de la URL
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
 $per_page = isset($_GET['per_page']) ? $_GET['per_page'] : 10;
 
-// Lógica de paginación
+// Lógica de la paginación
 $start = ($page - 1) * $per_page;
 $paginated_courses = array_slice($cursos, $start, $per_page);
 
-// Formar la respuesta paginada
 $response = array(
     'total' => count($cursos),
     'page' => $page,
@@ -40,6 +39,6 @@ $response = array(
     'data' => $paginated_courses
 );
 
-// Devolver la respuesta como JSON
+// Devolver la respuesta como JSON para poder verlo
 header('Content-Type: application/json');
 echo json_encode($response);
